d5d2ecf1ed87bcdb78ff59b8851d24aa41ae33db
Kadi Kraman <hellokadi@gmail.com>
2024-08-12 19:00:05 -0500

    Add a custom font

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
@@ -53,5 +53,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: theme.colorWhite,
     textAlign: "center",
+    fontFamily: Platform.select({
+      ios: "Caveat-Regular",
+      android: "Caveat_400Regular",
+    }),
   },
 });
